3a924ac257404fb06a05b45b03671ff5e642412b
Jia <jia.tian@me.com>
2018-05-06 14:29:15 +0800

    add json pretty

--- pages/json.vue
+++ pages/json.vue
@@ -1,5 +1,60 @@
 <template>
     <div>
-        
+        <h5>JSON Tree View</h5>
+        <div class="form">
+            <div class="form-group">
+                <textarea class="form-control" v-model="input" placeholder="input here" cols="30" rows="10"></textarea>
+                <p class="help-block alert alert-warning" v-if="message">
+                    {{message}}
+                </p>
+            </div>
+            <div class="form-group">
+                <button @click="Pretty" class="btn btn-primary">Show</button>
+            </div>
+        </div>
+        <p>
+            <vue-json-pretty
+                :data="jsonSource">
+            </vue-json-pretty>
+        </p>
     </div>
 </template>
+<script>
+//import Vue from 'vue'
+import VueJsonPretty from 'vue-json-pretty'
+// Vue.use(JsonTreeView);
+export default {
+    data() {
+        return {
+            input: "{}",
+            jsonSource: "{}",
+            message: "",
+        }
+    },
+    components: {
+        VueJsonPretty
+    },
+    methods: {
+        show() {
+            if (this.input.length == "") {
+                this.showMessage("input empty");
+            }
+            var json;
+            try {
+                json = JSON.parse(this.input);
+            } catch(e) {
+                this.showMessage("json data error.");
+                return
+            }
+            this.input = JSON.stringify(json);
+            this.jsonSource = json;
+        },
+        showMessage(message) {
+            this.message = message;
+            setTimeout(() => {
+                this.message = "";
+            }, 5000);
+        }
+    }
+}
+</script>
