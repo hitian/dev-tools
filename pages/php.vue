82c799c82422212965b96bff69063bbaddde1ab1
Jia <jia.tian@me.com>
2018-05-10 12:40:40 +0800

    add click copy to result box.

--- pages/php.vue
+++ pages/php.vue
@@ -4,17 +4,15 @@
         <div class="form">
             <div class="form-group">
                 <textarea class="form-control" v-model="input" placeholder="input here" cols="30" rows="10"></textarea>
-                <p class="help-block alert alert-warning" v-if="message">
-                    {{message}}
-                </p>
             </div>
             <div class="form-group">
                 <button @click="decode" class="btn btn-primary">decode</button>
                 <button @click="encode" class="btn btn-warning ml-5">encode</button>
-                
+                <span v-if="message" class="badge badge-warning ml-3">{{message}}</span>
+                <span v-if="copy_success" class="badge badge-success ml-3">result copied to clipboard</span>
             </div>
             <div class="form-group">
-                <textarea class="form-control" v-model="output" placeholder="result here" cols="30" readonly rows="10"></textarea>
+                <textarea class="form-control" @click="copyResult" v-model="output" placeholder="result here" cols="30" readonly rows="10"></textarea>
             </div>
         </div>
     </div>
@@ -27,7 +25,8 @@ export default {
         return {
             message: "",
             input: "",
-            output: ""
+            output: "",
+            copy_success: false,
         }
     },
     methods: {
@@ -35,7 +34,7 @@ export default {
             if (this.input.length == "") {
                 this.showMessage("input empty");
             }
-
+            this.message = "";
             var json;
             try {
                 json = JSON.parse(this.input);
@@ -55,25 +54,50 @@ export default {
             if (this.input.length == "") {
                 this.showMessage("input empty");
             }
-
+            this.message = "";
             let input = this.input;
             if (input.startsWith('"') && input.endsWith('"') && input.indexOf('\"') != -1) {
                 input = eval(input);
             }
             console.log(input);
-
-            let result = unserialize(input);
-            if (result) {
-                this.output = JSON.stringify(result);
-            } else {
-                this.showMessage("decode failed.");
+            try {
+                let result = unserialize(input);
+                if (result) {
+                    this.output = JSON.stringify(result);
+                    return;
+                } else {
+                    this.showMessage("decode failed.");
+                }
+            }catch(e){
+                this.showMessage("decode failed:" + e);
             }
+            
         },
         showMessage(message) {
             this.message = message;
             setTimeout(() => {
                 this.message = "";
             }, 5000);
+        },
+        showCopySuccess() {
+            this.copy_success = true;
+            setTimeout(() => {
+                this.copy_success = false;
+            }, 5000);
+        },
+        copyResult(event) {
+            let resultBox = event.target;
+            resultBox.focus();
+            resultBox.select();
+            try {
+                var isCopySuccess = document.execCommand('copy');
+                if (isCopySuccess) {
+                    this.showCopySuccess();
+                    return;
+                }
+            } catch(err) {  
+                console.log('Oops, unable to copy: ' + err);  
+            }
         }
     }
 }
