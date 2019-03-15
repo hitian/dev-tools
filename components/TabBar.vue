b2c4aeb9a49effbaba0f1cab489a0576d229f26b
Jia <jia.tian@me.com>
2019-03-15 17:56:29 +0800

    light mode.

--- components/TabBar.vue
+++ components/TabBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
         <nuxt-link class="navbar-brand" to="/">Dev-tools</nuxt-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
